a298dc2120f8b6ad47504ff2b5d8f467b12240c1
Jimin <jiminseo@brent.edu.ph>
2022-03-29 21:32:00 +0800

    updated errors

--- android/app/src/main/java/com/example/scanner/HomeActivity.java
+++ android/app/src/main/java/com/example/scanner/HomeActivity.java
@@ -2,21 +2,73 @@ package com.example.scanner;
 
 import static androidx.core.content.ContextCompat.startActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class HomeActivity {
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class HomeActivity extends Activity {
+
+    ImageView homeImage;
+    Button cameraBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.class);
-                startActivity(intent);
-            }
-        },200); //run after 200 milli sec
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        homeImage = (ImageView) findViewById(R.id.homeScreenBack); //view.findViewbyID
+        cameraBtn = (Button) findViewById(R.id.buttonCamera);
+
+        //request for camera acesss
+        if (ContextCompat.checkSelfPermission(
+                HomeActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) { // != "not"
+            // if camera is not granted then the following code will follow up for request permissions
+            ActivityCompat.requestPermissions(HomeActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+
+            // press button to move to Camera
+            cameraBtn.setOnClickListener(new View.OnClickListener() {
+                @Override // when there is a parent class, the child kid receives all the ]
+                // information, so when the child class wants to rewrite the code, we use "Override"
+                public void onClick(View view) {
+                    // open camera
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 100);
+                }
+            });
+
+
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(String.valueOf(this));
+                    startActivity(intent);
+                }
+            }, 200); //run after 200 milli sec
+        }
+    }
+    protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data)
+    {
+        if (requestCode == 100) {
+            //get capture image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data"); //bitmap is a from of image
+            //Set Capture Image to ImageView
+            homeImage.setImageBitmap(captureImage);
+        }
     }
 }
